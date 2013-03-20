74682d66738a0730c91fc24c79e95aa9ff64989f
Chris Gordon <chrisg@groupon.com>
2013-03-20 14:10:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photo::Application.config.secret_token = '890f5bd4dbcc820189719ef837890813d0b5d4d46f0ce5a769b23781c33912c1da6dcbc20789b29c0263fc7c81d6515e238654fb7fe30ef803eab3c5106271f9'
