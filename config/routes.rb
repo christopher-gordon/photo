432a99281cf3a1042048503cd163b8dc5a5b5089
Chris Gordon <ChristopherScottGordon@gmail.com>
2013-03-29 14:09:42 -0500

    Create Gallery controller and initial view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Photo::Application.routes.draw do
   get "home/index"
 
+  resources :gallery, :only => [:index]
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
