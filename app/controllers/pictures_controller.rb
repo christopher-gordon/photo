8c98458d99a284bcc1f76a17551b5f0e2c233340
Chris Gordon <ChristopherScottGordon@gmail.com>
2013-06-13 17:45:22 -0500

    more setup for create picture action, new view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,6 +6,9 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
+  def show
+  end
+
   def create
     picture = Picture.new
 
@@ -15,13 +18,23 @@ class PicturesController < ApplicationController
     picture.title = params[:picture][:title]
     picture.description = params[:picture][:description]
     picture.location = params[:picture][:location]
-    #picture.slideshow
-    #file upload
+    #picture.slideshow   #boolean
+    #upload params[:picture][:photo_file]
+    #upload params[:picture][:thumbnail_file]
 
-    #picture.save!
+    picture.save!
     puts picture.inspect
     puts "$$$$$$$$$$$$"
 
     redirect_to gallery_index_url #TODO: redirect to show page for newly added photo
   end
+
+  private
+
+  def upload(image)
+    File.open(Rails.root.join('public', 'uploads', image.original_filename), 'w') do |file|
+      file.write(image.read)
+    end
+  end
+
 end
