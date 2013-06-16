01b463c35c5ddd554eea2517548b57a707d7743d
Chris Gordon <ChristopherScottGordon@gmail.com>
2013-06-16 15:09:37 -0500

    set up valid_params? in Picture controller

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -10,32 +10,29 @@ class Picture < ActiveRecord::Base
   protected
 
   def self.create(params)
-    puts "CREATE"
-    return false unless valid_params?
+    return false unless valid_params?(params)
 
     picture = Picture.new
-
-    puts "PARAMS"
-    puts params.inspect
 
     picture.title = params[:title]
     picture.description = params[:description]
     picture.location = params[:location]
-    #picture.content_type = params[:photo_file].content_type
-    #picture.filename = params[:photo_file].original_filename
+    picture.content_type = params[:photo_file].content_type
+    picture.filename = params[:photo_file].original_filename
     picture.slideshow = (params[:slideshow] == 0 ? false : true)
 
-    #upload(params[:photo_file])
-    #upload(params[:thumbnail_file])
+    upload(params[:photo_file])
 
-    #picture.save!
+    picture.save!
   end
 
   private
 
-  def self.valid_params?
-    #TODO
-    return true
+  def self.valid_params?(params)
+    return false if params[:title].nil?
+    return false if params[:description].nil?
+    return false if params[:location].nil?
+    return false if params[:photo_file].nil?
   end
 
   def self.upload(image)
--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe PicturesController do
-
+  context "#create" do
+    it "creates a new Picture" do
+      #post :create, :picture => 
+    end
+  end
 end
