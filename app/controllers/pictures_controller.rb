29030ef34519c95e22753b502f9b918fb625281a
Chris Gordon <ChristopherScottGordon@gmail.com>
2013-06-16 13:56:17 -0500

    Picture - create form file upload, controller and model modifications and validations

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,33 +7,19 @@ class PicturesController < ApplicationController
   end
 
   def show
+    #@picture = 
   end
 
   def create
-    picture = Picture.new
+    puts "create controller"
+    picture_params = params[:picture]
 
-    puts "PARAMS"
-    puts params.inspect
-
-    picture.title = params[:picture][:title]
-    picture.description = params[:picture][:description]
-    picture.location = params[:picture][:location]
-    #picture.slideshow   #boolean
-    #upload params[:picture][:photo_file]
-    #upload params[:picture][:thumbnail_file]
-
-    picture.save!
-    puts picture.inspect
-    puts "$$$$$$$$$$$$"
-
-    redirect_to gallery_index_url #TODO: redirect to show page for newly added photo
-  end
-
-  private
-
-  def upload(image)
-    File.open(Rails.root.join('public', 'uploads', image.original_filename), 'w') do |file|
-      file.write(image.read)
+    if Picture.create(picture_params)
+      flash[:notice] = "You have added this picture!"
+      redirect_to gallery_index_url #TODO: redirect to show page for newly added photo
+    else
+      flash[:error] = "There was an error adding this picture!"
+      redirect_to gallery_index_url #TODO: redirect to pictures new page?
     end
   end
 
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,48 @@
 class Picture < ActiveRecord::Base
-  attr_accessible :description, :filename, :content_type, :location
+  #TODO - migration - remove thumbnail
+  attr_accessible :description, :title, :filename, :content_type, :location, :slideshow
 
-  has_one :album
+  #has_one :album #TODO albums
 
-  validates_presence_of :title, :description, :location
+  validates_presence_of :title, :description, :location, :filename, :content_type
   validates :filename, :uniqueness => true
+
+  protected
+
+  def self.create(params)
+    puts "CREATE"
+    return false unless valid_params?
+
+    picture = Picture.new
+
+    puts "PARAMS"
+    puts params.inspect
+
+    picture.title = params[:title]
+    picture.description = params[:description]
+    picture.location = params[:location]
+    #picture.content_type = params[:photo_file].content_type
+    #picture.filename = params[:photo_file].original_filename
+    picture.slideshow = (params[:slideshow] == 0 ? false : true)
+
+    #upload(params[:photo_file])
+    #upload(params[:thumbnail_file])
+
+    #picture.save!
+  end
+
+  private
+
+  def self.valid_params?
+    #TODO
+    return true
+  end
+
+  def self.upload(image)
+    puts "in upload"
+    File.open(Rails.root.join('app', 'assets', 'images', image.original_filename), 'wb') do |file|
+      file.write(image.read)
+    end
+  end
+
 end
