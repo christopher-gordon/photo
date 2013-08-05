9411fcd3d228bfa7139b8bb05b5c6f872114788f
Chris Gordon <ChristopherScottGordon@gmail.com>
2013-08-04 20:38:28 -0500

    gallery styling setup

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -3,5 +3,11 @@ class GalleryController < ApplicationController
     @albums = Album.all
 
     album = Album.find_by_name(params[:album]) if params[:album]
+
+    if params[:album]
+      @pictures = Picture.where("album_id = ?", album.id)
+    else
+      @pictures = Picture.all
+    end
   end
 end
