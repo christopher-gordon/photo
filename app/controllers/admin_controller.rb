6472e10229ec3c8394ba86e132b9e668d98e2529
Chris Gordon <ChristopherScottGordon@gmail.com>
2013-08-21 23:20:21 -0500

    add album_ordering instance variable to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,7 +10,16 @@ class AdminController < ApplicationController
 
   def order
     @albums = Album.all.select{|a| a.pictures.count > 0}
+
     @album = Album.find_by_name(params[:album]) if params[:album]
     @pictures = Picture.where("album_id = ?", @album.id) if params[:album]
+    generate_album_ordering if params[:album]
+  end
+
+  private
+
+  def generate_album_ordering
+    @album_orderings = []
+    @pictures.each {|picture| @album_orderings << picture.ordering }
   end
 end
