e87af70ce8f51875ac59fd70360d33c1bd48fe29
Chris Gordon <ChristopherScottGordon@gmail.com>
2014-04-16 21:32:16 -0500

    fix for adding "testing" to albums not displayed

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,7 +2,7 @@ class GalleryController < ApplicationController
   skip_before_filter :require_login
 
   def index
-    @albums = Album.all.select{|a| a.pictures.count > 0}.reject{|a| a.name == "None" || a.name == 'Frames' || a.name == 'Testing'}}
+    @albums = Album.all.select{|a| a.pictures.count > 0}.reject{|a| a.name == "None" || a.name == 'Frames' || a.name == 'Testing'}
     @album = Album.find_by_name(params[:album]) if params[:album]
     @pictures = Picture.where("album_id = ?", @album.id) if params[:album]
   end
