5d3ccffcd51be50ade2e8015b5a49175aea608e9
Chris Gordon <ChristopherScottGordon@gmail.com>
2014-07-19 15:21:54 -0500

    put min-widths on items in pictures/show; fix overlap of text and photo, multiline break of copy/links

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -3,7 +3,7 @@ module PicturesHelper
     if @album.name.downcase == 'frames'
       "Back to Frames"
     elsif cookies[:gallery].present?
-      "Back to #{@album.name} album"
+      "Back to #{@album.name}"
     else
       "Back to Gallery"
     end
