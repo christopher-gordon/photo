99e2eb5349635d260b42b4326793237c262c9304
Chris Gordon <ChristopherScottGordon@gmail.com>
2015-10-15 19:57:49 -0500

    alphabetize HomeController

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,11 @@ class HomeController < ApplicationController
     @posts = Post.all.sort_by(&:created_at).reverse!
   end
 
-  def index
-    @pictures = Picture.where("slideshow = ?", true).sort_by{|p| p.album.ordering}
-  end
-
   def framing
     @pictures = Picture.all.select{|pic| pic.album.name.capitalize == "Frames"}
+  end
+
+  def index
+    @pictures = Picture.where("slideshow = ?", true).sort_by{|p| p.album.ordering}
   end
 end
