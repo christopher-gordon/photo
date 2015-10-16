9081f301ab08516972ac074d5955be93ab4e68b4
Chris Gordon <ChristopherScottGordon@gmail.com>
2015-10-15 20:13:14 -0500

    handle blog copy and return link in picture/show

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -2,10 +2,23 @@ module PicturesHelper
   def back_to_gallery_or_album_copy
     if @album.name.downcase == 'frames'
       "Back to Frames"
+    elsif @album.name.downcase == 'blog'
+      "Back to Blog"
     elsif cookies[:gallery].present?
       "Back to #{@album.name}"
     else
       "Back to Gallery"
+    end
+  end
+
+  def previous_page_path
+    case @album.name.downcase
+    when "frames"
+      framing_path
+    when "blog"
+      blog_path
+    else
+      gallery_index_path
     end
   end
 end
