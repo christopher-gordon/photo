40ff3528aa13668cf3f7f8f0eba071538a8a62f2
Chris Gordon <ChristopherScottGordon@gmail.com>
2015-10-19 13:11:44 -0700

    WIP: add blog layouts and start adding tests

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe AdminController do
 
+  describe "#create" do
+    it "sets the instance variable pictures" do
+      4.times do
+        Picture.create(:
+      end
+    end
+  end
+
 end
