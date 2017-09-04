a94dea931bccf75a757da83f125dbbd85052bf5d
Chris Gordon <ChristopherScottGordon@gmail.com>
2017-09-04 16:41:46 -0500

    redirect to admin index after creating volume

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -9,7 +9,7 @@ class VolumesController < ApplicationController
 
     if Volume.create(:name => volume_params["name"])
       flash[:notice] = "You have successfully added this blog volume!"
-      redirect_to blog_path
+      redirect_to admin_index_path
     else
       flash[:error] = "There was an error adding the blog volume!  Please try again."
       redirect_to new_volume_path
